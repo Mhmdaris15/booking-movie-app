e6e29740a149a3aaf4f9e34ec38372bd16c107f5
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2023-06-21 03:27:22 +0700

    Basic Architecture Done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/Mhmdaris15/booking-movie-app/internal/routes"
+	"github.com/Mhmdaris15/booking-movie-app/pkg/database/mongo"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+        // Load environment variables from .env file
+        // err := godotenv.Load()
+        // if err != nil {
+        //         log.Fatal("Error loading .env file")
+        // }
+
+        // Connect to MongoDB
+        if err := mongo.Connect("mongodb://localhost:27017", "moviedb"); err != nil {
+                log.Fatal("Error connecting to MongoDB", err.Error())
+        }
+
+
         router := gin.Default()
 
-        router.GET("/", func(c *gin.Context) {
-                c.JSON(200, gin.H{
-                        "data": "Hello from Gin-gonic & mongoDB",
-                })
-        })
+        routes.SetupRoutes(router)
 
-        router.Run("localhost:6000") 
+        if err := router.Run(":3000"); err != nil {
+                log.Fatal(err.Error())
+        }
 }
